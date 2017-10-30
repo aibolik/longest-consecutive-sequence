3d5c111bf64fb759e3ebc9fd7289155cdc951109
Aibol Kussain <Aibol_Kussain@epam.com>
2017-10-30 12:19:57 +0300

    submit: solution of Aibol Kussain(recall to issue #1 due to test failure)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+	var arr = []
+	for (var i in array) {
+		if (Number.isInteger(arr[array[i]])) {
+			arr[array[i]]++
+		} else {
+			arr[array[i]] = 1
+		}
+	}
+	var longest = 0
+	var currentLong = 1
+	var lastIt
+	for (var i in arr) {
+		if (i === 0) {
+			lastIt = i
+			continue
+		}
+		if (i - lastIt === 1) {
+			currentLong++
+		} else {
+			longest = longest < currentLong ? currentLong : longest
+			currentLong = 1
+		}
+		lastIt = i
+	}
+	longest = longest < currentLong ? currentLong : longest
+	return longest
 }
